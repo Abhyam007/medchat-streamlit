8af00938acf801f656bb4e4af85df7775c8e8d41
Abhyam007 <abhyam007@gmail.com>
2026-04-14 17:49:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 class Config:
-    GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+    GEMINI_API_KEY = st.secrets.get("GEMINI_API_KEY", None)
+
     DB_PATH = "vectorstore/db_faiss"
     CSV_PATH = "data/medical.csv"
     TXT_PATH = "data/medical.txt"
